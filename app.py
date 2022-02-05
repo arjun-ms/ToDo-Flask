651661044b4df731bd02c04ba2578f178fd944bc
arjun-ms <arjunachu123.aa@gmail.com>
2022-02-05 12:11:42 +0530

    Add Delete fn

--- app.py
+++ app.py
@@ -1,10 +1,63 @@
-from flask import Flask , render_template
+from flask import Flask, redirect , render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255),nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route('/', methods=['POST','GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        # gets the content from the input field
+        task_content = request.form['content']
+        # passing task_content to content field of Todo table
+        new_task = Todo(content=task_content)
+
+        try:
+            # adding new_task to db
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+    
+    else:
+        # odering all tasks by date_created
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html',tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    # search for the task by id 
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+
+    except:
+        return 'There was an error deleting the task'
+
+# @app.route('/update/')
+# def update(id,content):
+#     task_to_update = Todo.query.get_or_404(id)
+
+#     try:
+#         db.sess
 
 if __name__ == '__main__':
     app.run(debug=True)
