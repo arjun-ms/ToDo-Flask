53d480603d1b3a30e0645f22aa92490865e9a62e
arjun-ms <arjunachu123.aa@gmail.com>
2022-02-06 08:23:45 +0530

    Exclude test.db

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
